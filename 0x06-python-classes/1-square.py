2862b8a9b2dfd40097be25a7580d58b204e43c56
Ordu <ordudesigns@gmail.com>
2023-12-19 12:56:32 -0800

    classes

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Defines a Square"""
+
+class square:
+    """represents a square"""
+
+    def __init__(self, size):
+        """initializing this square class
+        Args: size - represnets the size of the square defined
+        """
+
+        self.__size = size
